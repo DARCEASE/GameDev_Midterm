2e593ac24618adac96419a4365818bc981c56538
Darcy <darcelisgutierrez@gmail.com>
2020-10-06 09:16:07 -0400

    Icon Rotation
    icons spin around the center figure

--- Assets/Scripts/GroundedCheck.cs
+++ Assets/Scripts/GroundedCheck.cs
@@ -13,6 +13,31 @@ public class GroundedCheck : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        // if we are on the ground, then do nothing
+
+        // 1. declare a Ray variable
+        Ray2D myRay = new Ray2D(transform.position, Vector2.down);
+
+        // 2. define the maximum Raycast distance
+        float maxRayDist = 0.6f;
+
+        // 3. visualize the raycast in the Scene view (ONLY YOU CAN SEE THIS)
+        Debug.DrawRay(myRay.origin, myRay.direction * maxRayDist, Color.green);
+
+        // 4. actually fire the raycast now!
+        RaycastHit2D myRayHit = Physics2D.Raycast(myRay.origin, myRay.direction, maxRayDist);
+
+        // 5. see if the raycast actually hit something?
+        if (myRayHit.collider != null)
+        {
+            // do nothing
+            Debug.Log("Raycast is hitting " + myRayHit.collider.name);
+        }
+        else
+        {
+            // ... but if we're in the air, then spin
+            transform.Rotate(0, 0, 30f * Time.deltaTime); // spin 30 degrees per second
+        }
+
     }
 }
