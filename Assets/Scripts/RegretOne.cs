6e909815229ce3d7cb3712d83b2cf29d7d21bdb1
Darcy <darcelisgutierrez@gmail.com>
2020-10-12 17:59:57 -0400

    Update Sprites in scene one + Time of day detected in scene 2

--- Assets/Scripts/RegretOne.cs
+++ Assets/Scripts/RegretOne.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 //purpose: game manager to RegretOne 
+//usage: place this on an empty game object and drag it into the scene 
 public class RegretOne : MonoBehaviour
 {
     public Sprite[] compScreen; //list of images on the screen of the computer 
@@ -27,17 +28,17 @@ public class RegretOne : MonoBehaviour
         //on a timer the time of day should change into the next phase on the window
         dayTimer += Time.deltaTime;
         timeChngSpr.GetComponent<SpriteRenderer>().sprite = timeOD[timeID];
-        if (dayTimer > 2)
+        if (dayTimer > 10)
         {
             //
             dayTimer = 0;
-            if (timeID == 2)
+            if (timeID != 2) //if the timeID is anything but element 2, keep going. 
             {
-                timeID = 0;
-            }
-            else
-            { 
                 timeID += 1;
+            }
+            else if (timeID == 2) // but if it is element then stop the array 
+            { 
+               
             }
         }
         //when i right click, the image on the computer should change 
@@ -48,6 +49,7 @@ public class RegretOne : MonoBehaviour
             
         }
         // on a timer, the phone on the desk should randomly vibrate 
+
 
         
     }
