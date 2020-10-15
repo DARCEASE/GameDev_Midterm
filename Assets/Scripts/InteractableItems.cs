53cf995505a6899cccf2c232a4046a3d1372d992
Darcy <darcelisgutierrez@gmail.com>
2020-10-15 16:31:02 -0400

    Interactable items set up

---
+++ Assets/Scripts/InteractableItems.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractableItems : MonoBehaviour
+{
+    public GameObject handSanitizer;
+    public Renderer rend;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnMouseEnter()
+    {
+        rend.material.color = Color.red;
+    }
+
+    // ...the red fades out to cyan as the mouse is held over...
+    void OnMouseOver()
+    {
+        rend.material.color -= new Color(0.1F, 0, 0) * Time.deltaTime;
+    }
+
+    // ...and the mesh finally turns white when the mouse moves away.
+    void OnMouseExit()
+    {
+        rend.material.color = Color.white;
+    }
+}
